63b6a7f97cab2ab9ef9133d42f9fb010620aec92
seunggi-coding <moon010103@naver.com>
2022-06-27 16:08:54 +0900

    codeit

---
+++ Numpy/Numpy1.py
@@ -0,0 +1,22 @@
+import numpy
+
+array1 = numpy.array([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31])
+print(array1)
+print(type(array1))\
+
+# 요소의 개수
+print(array1.size)
+
+# 행열로 표시
+print(array1.shape)
+
+print("\n")
+
+array2 = numpy.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+print(array2)
+print(type(array2))
+
+# 요소의 개수
+print(array2.size)
+# 행열로 표시
+print(array2.shape)
---
+++ Numpy/Numpy_array.py
@@ -0,0 +1,9 @@
+# 1부터 100까지(1과 100 포함) 담겨 있는 numpy array를 생성하고 출력해 주세요.
+
+import numpy as np
+
+# 코드를 작성하세요.
+arr = np.arange(1, 101)
+
+# 정답 출력
+print(arr)
