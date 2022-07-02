5a4e4cdb9eb6e33b2d2034761e3780f78a4618c9
seunggi-coding <moon010103@naver.com>
2022-07-02 13:14:13 +0900

    Contest

--- Contest/CarbonNeutrality_EastWestPower/CarbonNeutrality_EastWestPower.py
+++ Contest/CarbonNeutrality_EastWestPower/CarbonNeutrality_EastWestPower.py
@@ -3,11 +3,12 @@ from Func import month, Month_day as md, Five_VehicleSystem as vs, People_car as
 
 i = random.randint(0, 3)
 list_park = pc()
+pu_transport = ""
+
 print("오늘은 {}월 입니다. {}월은 {}일까지 있습니다.\n".format(month, month, md()))
 day2 = input("오늘은 몇일인가요? ")
+print("\n")
 
 for j in range(4) :
     ex_park = list_park[j]
-    print(ex_park)
-    vs(day2, ex_park[0], ex_park[1], ex_park[2])
-    print("\n")
+    vs(day2, ex_park[0], ex_park[1], ex_park[2], pu_transport)
